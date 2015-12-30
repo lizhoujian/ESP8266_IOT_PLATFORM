f70073fe434d2fe13f8db3469e2715ffc5409828
zjli <lizhoujian123@613.com>
2015-12-30 11:43:16 +0800

    remove obj file.

--- user/.output/eagle/debug/obj/user_sensor.d
+++
@@ -1,1 +0,0 @@
-.output/eagle/debug/obj/user_sensor.o .output/eagle/debug/obj/user_sensor.d : user_sensor.c ../include/user_config.h
--- user/.output/eagle/debug/obj/user_webserver.d
+++
@@ -1,2 +0,0 @@
-.output/eagle/debug/obj/user_webserver.o .output/eagle/debug/obj/user_webserver.d : user_webserver.c ../include/user_webserver.h \
- ../include/user_config.h
